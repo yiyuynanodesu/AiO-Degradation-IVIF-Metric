354fe272e86d36013254464866095eff696e64ed
yiyuynanodesu <t-wanghaoyang@beyondsoft.com>
2025-12-07 15:51:11 +0800

    update code

--- Tool/classify_image.py
+++ Tool/classify_image.py
@@ -3,6 +3,9 @@ import os
 
 """
 对 Light DDL-12中的图像进行分类 主要分为光照退化 及 天气退化 对于雾+低光单独拿出来
+天气（雨，雾，雨+雾，雾+低光 使用OneRestore恢复)
+光照（低光，高曝 使用InstructIR恢复）
+参考ControlFusion
 """
 
 dataset_path = './dataset/Light_DDL-12'
@@ -24,13 +27,8 @@ for phrase in os.listdir(dataset_path):
         os.makedirs(weather_target_phrase_image_type, exist_ok=True)
         light_target_phrase_image_type = os.path.join(target_phrase_image_type, "light")
         os.makedirs(light_target_phrase_image_type, exist_ok=True)
-        both_target_phrase_image_type = os.path.join(target_phrase_image_type,"both")
-        os.makedirs(both_target_phrase_image_type, exist_ok=True)
 
         for file in os.listdir(dataset_phrase_image_type):
-            if "Haze" in file and "Low" in file:
-                shutil.copy(os.path.join(dataset_phrase_image_type, file), os.path.join(both_target_phrase_image_type, file))
-                continue
             if "Rain" in file or "Haze" in file:
                 shutil.copy(os.path.join(dataset_phrase_image_type, file), os.path.join(weather_target_phrase_image_type, file))
                 continue
