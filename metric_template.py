58811eed29506aeb273d8608129a9357f0e8a434
yiyuynanodesu <t-wanghaoyang@beyondsoft.com>
2025-12-08 22:18:03 +0800

    update code

--- metric_template.py
+++ metric_template.py
@@ -2,21 +2,23 @@ from Metric.eval_metric import eval_batch as normal_eval
 from Metric.degrad_eval_metric import eval_bath as degrad_eval
 from Metric.degrad_eval_metric_by_type import eval_batch as degrad_type_eval
 
+model = ''
+
 # Normal IVIF
 ir_path = ''
 vis_path = ''
 output_path = ''
-save_dir = ''
+save_dir = f'./{model}/'
 
-normal_eval(ir_path, vis_path, output_path, save_dir, model_name='Model', excel_filename='metric.xlsx')
+normal_eval(ir_path, vis_path, output_path, save_dir, model_name=model, excel_filename=f'metric_{model}_normal.xlsx')
 
 # Degrad IVIF
 
 output_path = ''
 save_dir = ''
-degrad_eval(output_path, save_dir, model_name='Model', excel_filename='metric.xlsx')
+degrad_eval(output_path, save_dir, model_name=model, excel_filename=f'metric_{model}_degrad.xlsx')
 
 # Degrad Type IVIF
 output_path = ''
 save_dir = ''
-degrad_type_eval(output_path, save_dir, degard_list=['HazeRain','HazeLow','Rain','Haze','Exposure','Light'] model_name='Model', excel_filename='metric.xlsx')
+degrad_type_eval(output_path, save_dir, degard_list=['HazeRain','HazeLow','Rain','Haze','Exposure','Light'] model_name=model, excel_filename=f'metric_{metric}_detail.xlsx')
