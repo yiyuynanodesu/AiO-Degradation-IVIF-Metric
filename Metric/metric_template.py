1eb7a780fd0287d2a64fb360f81a255da1ec5f34
yiyuynanodesu <t-wanghaoyang@beyondsoft.com>
2025-12-12 18:38:05 +0800

    update

--- Metric/metric_template.py
+++ Metric/metric_template.py
@@ -7,21 +7,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description='PyTorch Training Example')
 parser.add_argument('--model', default = '', help='model')
-parser.add_argument('--ir_path', default = '', help='model')
-parser.add_argument('--vi_path', default = '', help='model')
+parser.add_argument('--dataset', default = '', help='dataset')
+parser.add_argument('--ir_path', default = '', help='ir path')
+parser.add_argument('--vi_path', default = '', help='vi path')
 parser.add_argument('--result_path', default = '', help='result path')
 parser.add_argument('--save_path', default = './', help='model')
 args = parser.parse_args()
 
-# Normal IVIF
-# normal_eval(args.ir_path, args.vi_path, args.result_path, args.save_path, model_name=args.model, excel_filename=f'{args.model}_DDL_normal.xlsx')
-
 # Degrad IVIF
-# DDL
-degrad_eval(args.result_path, args.save_path, model_name=args.model, excel_filename=f'{args.model}_degrad.xlsx')
-# Degrad Type IVIF
-degrad_type_eval(args.result_path, args.save_path, degard_list=['HazeRain','HazeLow','Rain','Haze','Exposure','Light'], model_name=args.model, excel_filename=f'{args.model}_DDL_detail.xlsx')
-
-# EMS
-# degrad_eval(args.result, args.save_path, model_name=args.model, excel_filename=f'{args.model}_EMS_degrad.xlsx')
-# degrad_type_eval(args.result, args.save_path, degard_list=['Rain','Haze','Exposure','Light'], model_name=args.model, excel_filename=f'{args.model}_EMS_detail.xlsx')
+if args.dataset == 'DDL':
+    # DDL
+    degrad_eval(args.result_path, args.save_path, model_name=args.model, excel_filename=f'{args.model}_{args.dataset}_degrad.xlsx')
+    # Degrad Type IVIF
+    degrad_type_eval(args.result_path, args.save_path, degard_list=['HazeRain','HazeLow','Rain','Haze','Exposure','Light'], model_name=args.model, excel_filename=f'{args.model}_{args.dataset}_detail.xlsx')
+elif args.dataset == 'EMS':
+    degrad_eval(args.result, args.save_path, model_name=args.model, excel_filename=f'{args.model}_{args.dataset}_degrad.xlsx')
+    degrad_type_eval(args.result, args.save_path, degard_list=['Rain','Haze','Exposure','Light'], model_name=args.model, excel_filename=f'{args.model}_{args.dataset}_detail.xlsx')
+else:
+    # Normal IVIF
+    normal_eval(args.ir_path, args.vi_path, args.result_path, args.save_path, model_name=args.model, excel_filename=f'{args.model}_{args.dataset}_normal.xlsx')
